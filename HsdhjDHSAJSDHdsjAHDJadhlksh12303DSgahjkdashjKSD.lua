d712716b41dd162576ffb311637042dff8be5aca
danlachoo <danlacho.so@gmail.com>
2024-07-26 21:43:40 +0300

    Update HsdhjDHSAJSDHdsjAHDJadhlksh12303DSgahjkdashjKSD.lua

--- HsdhjDHSAJSDHdsjAHDJadhlksh12303DSgahjkdashjKSD.lua
+++ HsdhjDHSAJSDHdsjAHDJadhlksh12303DSgahjkdashjKSD.lua
@@ -1,11 +1,11 @@
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 
 local Window = OrionLib:MakeWindow({
-    Name = "XlebHub",
+    Name = "RezCokk",
     HidePremium = false,
-    IntroText = "XlebHub",
+    IntroText = "RezCokk",
     SaveConfig = true,
-    ConfigFolder = "XlebHub"
+    ConfigFolder = "RezCokk"
 })
 
 local Tab = Window:MakeTab({
@@ -20,8 +20,8 @@ local LT2 = Window:MakeTab({
     PremiumOnly = false
 })
 
-local Section = LT2:AddSection({
-	Name = "Shops"
+local SectionShops = LT2:AddSection({
+    Name = "Shops"
 })
 
 LT2:AddButton({
@@ -43,7 +43,6 @@ LT2:AddButton({
         end
     end
 })
-
 
 LT2:AddButton({
     Name = "Car shop",
@@ -85,10 +84,9 @@ LT2:AddButton({
     end
 })
 
-local Section = LT2:AddSection({
-	Name = "Biomes"
+local SectionBiomes = LT2:AddSection({
+    Name = "Biomes"
 })
-
 
 LT2:AddButton({
     Name = "Swamp Location",
@@ -118,4 +116,111 @@ LT2:AddButton({
             player.Character.HumanoidRootPart.CFrame = CFrame.new(1938.3, -5.91243, -1537.41)
         end
     end
+})
+
+-- Adding features to the Main tab
+local SectionPlayer = Tab:AddSection({
+    Name = "Player"
+})
+
+SectionPlayer:AddSlider({
+    Name = "Walkspeed",
+    Min = 0,
+    Max = 100,
+    Default = 16,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    ValueName = "Speed",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+    end    
+})
+
+SectionPlayer:AddSlider({
+    Name = "Jump Power",
+    Min = 0,
+    Max = 200,
+    Default = 50,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    ValueName = "Power",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end    
+})
+
+SectionPlayer:AddSlider({
+    Name = "Field of View",
+    Min = 70,
+    Max = 120,
+    Default = 70,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    ValueName = "FOV",
+    Callback = function(Value)
+        game.Workspace.CurrentCamera.FieldOfView = Value
+    end    
+})
+
+local SectionCFrameFly = Tab:AddSection({
+    Name = "CFrame Fly"
+})
+
+SectionCFrameFly:AddToggle({
+    Name = "Enable Fly",
+    Default = false,
+    Callback = function(Value)
+        _G.Fly = Value
+        local Player = game.Players.LocalPlayer
+        local Character = Player.Character or Player.CharacterAdded:Wait()
+        local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+        
+        if Value then
+            -- Fly logic here
+            local FlySpeed = 2 -- Change the speed as needed
+            local flying = true
+            local BodyGyro = Instance.new("BodyGyro")
+            local BodyVelocity = Instance.new("BodyVelocity")
+
+            BodyGyro.P = 9e4
+            BodyGyro.Parent = HumanoidRootPart
+            BodyVelocity.Parent = HumanoidRootPart
+
+            while _G.Fly do
+                wait()
+                HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+                local Control = {F = 0, B = 0, L = 0, R = 0}
+                local MaxSpeed = FlySpeed
+                local Camera = game.Workspace.CurrentCamera
+
+                local LookVector = Camera.CFrame.lookVector
+                Control.F = (game.UserInputService:IsKeyDown(Enum.KeyCode.W) and 1 or 0)
+                Control.B = (game.UserInputService:IsKeyDown(Enum.KeyCode.S) and 1 or 0)
+                Control.L = (game.UserInputService:IsKeyDown(Enum.KeyCode.A) and 1 or 0)
+                Control.R = (game.UserInputService:IsKeyDown(Enum.KeyCode.D) and 1 or 0)
+
+                BodyVelocity.Velocity = ((LookVector * (Control.F - Control.B)) + ((Camera.CFrame * CFrame.new(Control.L - Control.R, 0, 0)).p - Camera.CFrame.p)) * MaxSpeed
+                BodyGyro.CFrame = Camera.CFrame
+            end
+
+            BodyGyro:Destroy()
+            BodyVelocity:Destroy()
+        else
+            -- Disable fly logic here
+            flying = false
+        end
+    end    
+})
+
+SectionCFrameFly:AddSlider({
+    Name = "Fly Speed",
+    Min = 1,
+    Max = 10,
+    Default = 2,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    ValueName = "Speed",
+    Callback = function(Value)
+        FlySpeed = Value
+    end    
 })
